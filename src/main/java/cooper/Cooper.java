47d5bf420e12798b4119a7d543fb085d8a7e837d
Rrraaaeee <qingxuannn0602@gmail.com>
2021-10-24 14:20:39 +0800

    added emulation of user input for demo

--- src/main/java/cooper/Cooper.java
+++ src/main/java/cooper/Cooper.java
@@ -1,14 +1,12 @@
 package cooper;
 
+import java.lang.Thread;
 import java.util.NoSuchElementException;
 
 import cooper.command.Command;
 import cooper.exceptions.InvalidAccessException;
 import cooper.exceptions.InvalidCommandFormatException;
-import cooper.finance.FinanceManager;
-import cooper.forum.ForumManager;
 import cooper.log.CooperLogger;
-import cooper.meetings.MeetingManager;
 import cooper.storage.StorageManager;
 import cooper.ui.Ui;
 import cooper.exceptions.UnrecognisedCommandException;
@@ -20,9 +18,11 @@ import cooper.resources.ResourcesManager;
 public class Cooper {
 
     private final ResourcesManager cooperResourcesManager;
+    private final CommandEmulator emulator;
 
     public Cooper() {
         cooperResourcesManager = new ResourcesManager();
+        emulator = new CommandEmulator();
         CooperLogger.setupLogger();
     }
 
@@ -54,7 +54,10 @@ public class Cooper {
         SignInDetails successfulSignInDetails = null;
         String input;
         while (!cooperVerifier.isSuccessfullySignedIn()) {
-            input = Ui.getInput();
+            input = emulator.typeNext();
+            if (input.length() == 0) {
+                input = Ui.getInput();
+            }
             successfulSignInDetails = cooperVerifier.verify(input);
         }
         assert successfulSignInDetails != null;
@@ -66,7 +69,10 @@ public class Cooper {
     private void runLoopUntilExitCommand(SignInDetails signInDetails) {
         while (true) {
             try {
-                String input = Ui.getInput();
+                String input = emulator.typeNext();
+                if (input.length() == 0) {
+                    input = Ui.getInput();
+                }
                 Command command = CommandParser.parse(input);
                 assert command != null;
                 command.execute(signInDetails, cooperResourcesManager);
@@ -79,6 +85,41 @@ public class Cooper {
             } catch (InvalidAccessException e) {
                 Ui.printNoAccessError();
             }
+        }
+    }
+}
+
+
+class CommandEmulator {
+    private final String[] commandSequence = {
+        "login me pw 12345 as admin",
+        "add 500",
+        "add (1200)",
+        "list"
+    };
+    private int counter;
+
+    public CommandEmulator() {
+        counter = 0;
+    }
+
+    public String typeNext() {
+        if (counter < 0 || counter >= commandSequence.length) {
+            return "";
+        }
+        try {
+            Ui.getInput(); // dummy line to only emulate after "enter"
+            String input = commandSequence[counter];
+            for (int i = 0; i < input.length(); i++) {
+                char c = input.charAt(i);
+                System.out.print(c);
+                Thread.sleep(50);
+            }
+            counter++;
+            return input;
+        } catch (InterruptedException e) {
+            // unforseen interrupt error, return empty string
+            return "";
         }
     }
 }
